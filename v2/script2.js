a730355a2cdda4aab3c6fd3e3f29e0f125113253
SandPiee <73396935+SandPiee@users.noreply.github.com>
2020-10-25 23:56:52 +0700

    Add files via upload

---
+++ v2/script2.js
@@ -0,0 +1,69 @@
+$('document').ready(function() {
+	
+	$('.none').on('click', function() {
+		alert('У этой кнопки нет назначения')
+	});
+	
+	$('#a1').on('click', function() {
+		$("#val1").add("1");
+	});
+	
+	$('#a2').on('click', function() {
+		var val1, val2, res;
+		val1 = $('#val1').val();
+		val2 = $('#val2').val();
+		
+		val1 = parseInt(val1);
+		val2 = parseInt(val2);
+		
+		res = val1 - val2;
+		$('#result').val(res);
+	});
+	
+	$('#a3').on('click', function() {
+		var val1, val2, res;
+		val1 = $('#val1').val();
+		val2 = $('#val2').val();
+		
+		val1 = parseInt(val1);
+		val2 = parseInt(val2);
+		
+		res = val1 / val2;
+		$('#result').val(res);
+	});
+	
+	$('#a4').on('click', function() {
+		var val1, val2, res;
+		val1 = $('#val1').val();
+		val2 = $('#val2').val();
+		
+		val1 = parseInt(val1);
+		val2 = parseInt(val2);
+		
+		res = val1 * val2;
+		$('#result').val(res);
+	});
+	
+	$('#a5').on('click', function() {
+		var val1, val2, res;
+		val1 = $('#val1').val();
+		val2 = $('#val2').val();
+		
+		val1 = parseInt(val1);
+		val2 = parseInt(val2);
+		
+		res = Math.pow(val1, val2);
+		$('#result').val(res);
+	});
+	
+	$('#clr').on('click', function() {
+		var clr
+		$('input').val(clr);
+	});
+
+	$('#round').on('click', function() {
+		var res = $('#result').val();
+		res = Math.round(res);
+		$('#result').val(res);
+	});
+});
